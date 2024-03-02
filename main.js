b6958e1a45bacc9389778355d435df2d610957b8
Hardyn1 <amki.business@gmail.com>
2024-03-02 15:34:41 +0200

    Js basics

---
+++ main.js
@@ -0,0 +1,167 @@
+// task 1
+
+var name = "artem";
+var age = 22;
+var position = "married"
+var child = "none"
+
+
+console.log(typeof name);
+
+console.log(typeof age);
+
+console.log(typeof position);
+
+console.log(typeof child);
+
+if (position = "married") {
+	console.log(true)
+}
+else  {
+	console.log(false)
+}
+
+// task 2
+
+var height = 23;
+var width = 10;
+var result = height 
+
+console.log(23*10);
+
+
+// task 3
+
+var height1 = 10;
+var cube = 4;
+var number = 3.14;
+
+console.log(number**cube*height1);
+
+// task 4
+
+console.log(Infinity - "1");  //infinity ?
+console.log("42" + 42);  //думаю это конкотенация строка к числу будет 4242 ?
+console.log(2 + "1 1") //тоже конкотенация ?      а не получилось 21 1
+console.log(99 + 101)  // процесс сложения = 200
+console.log("1" - "1") // посчитает их как строки ?  получилсч ответ 0
+console.log("Result: " + 10/2)  //result 5
+console.log(3 + " bananas " + 2 + " apples "); // может 3 бананов и  2 яблока 
+
+
+
+// console.log("Hello world !");
+
+// var number = 1;
+
+// var word = "any word"
+
+//  var a = 1; // number числа или дробные
+//  var b = "moscow"; //string строка
+//  var c = true; //boolean истина или ложь
+//  var d = null; //null  ничего или значение неизвестно
+//  var e; //undefined если переменанная обьявлена но там ничего не положили
+// ЭТО БЫЛИ 5 ПРИМИТИВНЫХ ТИПО ДАННЫХ
+
+// УЖЕ НЕ ПРИМИТИВНОЕ
+//  var f = {name: "Ivan"}; //object коллекция значений удобный тип данных с которым можно работать на протяжении всей программы
+
+
+
+// var user = {
+// 	userName : "Artjom",
+// 	age : 23,
+// 	position : married,
+// }
+
+
+
+// var name = "artem";
+// var age = 23;
+// var position = "married `engaged`";
+
+
+
+// if (position) {
+// 	console.log(true)
+// }
+// else {
+// 	console.log(false)
+// }
+
+
+
+
+//number
+//если нулей много и трудно посчитать то можно записать по другому
+// var n = 1e15;
+// один слева от четырех
+// var num = 1;
+// var num2 = 1.10;
+// var n = 1e-4;
+// console.log(n)
+
+
+// string
+// обратный slash / для строки 
+// var t = "ivan";
+// var text = "any text";
+// var words = "she says to him: `Hello` ";
+// console.log(words)
+
+
+// var t = true; //истина 1  могут представляться как 1 0
+// var n = false; // ложь 0
+
+
+// if (10<2) {
+// 	console.log(1)
+// }else {
+// 	console.log(2)
+// }
+
+// ПРОВЕРКА НА ТИП ДАННЫХ 
+
+// console.log(typeof a);
+
+// console.log(typeof f);
+
+// console.log(typeof c);
+
+// console.log(typeof d);
+
+// console.log(typeof e);
+
+// console.log(typeof f);
+
+// var num1 = 50;
+// var num2 = 50;
+// var summa = num1 * num2;
+
+// КОНКОТЕНАЦИЯ ПРОЦЕСС СКЛЕИВАНИЯ СТРОК  + он воспринимает как строку
+
+// var word1 = "Hello";
+// var word2 = "My";
+// var word3 = "Friend";
+// var result = word1 + word2 + word3;
+// console.log(result)
+
+// ОСТАТОК ОТ ДЕЛЕНИЯ 
+
+// console.log(10 % 7); //3
+
+// ВОЗВЕДЕНИЕ В СТЕПЕНЬ
+
+// console.log(5**2);
+
+
+// ИНКЕМЕНТ ИЛИ ДЕКРЕМНЕТ
+// ИНКЕМЕНТ УВЕЛИЧИВАЕТ НА ОДНУ
+// var b = 3;
+// console.log(++b);
+
+// // ДЕКРЕМЕНТ УМЕНЬШАЕТ НА ОДНУ
+
+// var a = 3;
+// console.log(--b);
+// b++ ЭТО ВСЕ РАВНО ЧТО B+1
