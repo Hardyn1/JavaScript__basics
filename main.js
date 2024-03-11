559ed7e86f6ad42bcca7814472ccaee853ed8b3f
Hardyn1 <amki.business@gmail.com>
2024-03-11 14:12:50 +0200

    lesson done

--- main.js
+++ main.js
@@ -1,53 +1,137 @@
-// task 1
+// 11 LESSON TASK 1 
 
-var name = "artem";
-var age = 22;
-var position = "married"
-var child = "none"
+// var name = "artem";
+// var age = 22;
+// var position = "married"
+// var child = "none"
 
 
-console.log(typeof name);
+// console.log(typeof name);
 
-console.log(typeof age);
+// console.log(typeof age);
 
-console.log(typeof position);
+// console.log(typeof position);
 
-console.log(typeof child);
+// console.log(typeof child);
 
-if (position = "married") {
-	console.log(true)
-}
-else {
-	console.log(false)
-}
+// if (position = "married") {
+// 	console.log(true)
+// }
+// else {
+// 	console.log(false)
+// }
 
 // task 2
 
-var height = 23;
-var width = 10;
-var result = height * width;
+// var height = 23;
+// var width = 10;
+// var result = height * width;
 
-console.log(result);
+// console.log(result);
 
 
 // task 3
 
-var h = 10;
-var s = 4;
+// var h = 10;
+// var s = 4;
 
-
-
-console.log(s ** h);
+// console.log(s ** h);
 
 // task 4
 
-console.log(Infinity - "1");  //infinity ?
-console.log("42" + 42);  //4242
-console.log(2 + "1 1") //21 1
-console.log(99 + 101)  // 200
-console.log("1" - "1") // 0
-console.log("Result: " + 10 / 2)  //result 5
-console.log(3 + " bananas " + 2 + " apples "); //3 бананов и  2 яблока 
+// console.log(Infinity - "1");  //infinity ?
+// console.log("42" + 42);  //4242
+// console.log(2 + "1 1") //21 1
+// console.log(99 + 101)  // 200
+// console.log("1" - "1") // 0
+// console.log("Result: " + 10 / 2)  //result 5
+// console.log(3 + " bananas " + 2 + " apples "); //3 бананов и  2 яблока 
+
+
+
+// var g = "Я"
+// var u = "М"
+// console.log(g>u)
+
+
+// console.log(undefined = null);
+
+// 12 LESSON TASK 1
+
+// Строгое равно проверяет на равенство две величины,
+//  при этом тип каждой из величин перед сравнением не изменяется (не приводится).
+//  Если значения имеют различающиеся типы, то они не могут быть равными.
+
+// СТРОГОЕ СРАВНЕНИЕ
+
+
+"42" == 42; true
+"0" == 0; true
+"0" == false; true
+"true" == true; false
+true == (1 == "1"); true
+
+"42" === 42; false
+"0" === 0; false
+"0" === false; false
+"true" === true; false
+true === (1 === "1"); false
+
+
+console.log();
+
+
+// TASK 2 
+
+var a = "foo"
+
+var b = "bar"
+
+console.log(a+ +b)
+// ВЫВЕЛО Foo + NaN означает что выполнена бессмысленная информация
+
+
+// TASK 3 
+
+// ("ананас" > "яблоко") false  сравнивается по символьно и по различию символов
+
+// undefined == null true потому что оба значения как бы отсутсвуют
+// undefined === null ; false
+
+// TASK 4
+
+
+// УСЛОВИЯ 
+
+// if(10>9) {
+// 	console.log("10 is bigger than 9");
+// } else {
+// 	console.log("10 is not bigger than 9")
+// }
+
+// var a = 50;
+
+// if(a>40) {
+// 	console.log("a is bigger than 40");
+// } else {
+// 	console.log("a is not bigger than 40")
+// }
+
+
+// ТЕРНАРНЫЙ ОПЕРАТОР
+
+// a = 10;
+// b = 20;
+// var result
+
+// // a > b ? result = "a > b": result = "a < b ";     A < B ЭТО FALSE
+// console.log(result);
+
+
+
+
+
+
 
 
 
